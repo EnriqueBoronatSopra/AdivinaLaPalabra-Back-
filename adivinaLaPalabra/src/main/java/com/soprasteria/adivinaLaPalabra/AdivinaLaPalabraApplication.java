c5e192bec8f6991baf8abb2e1de6e327d3e0352f
PONCE Enrique Javier <ej.boronat@ext.soprasteria.com>
2023-04-18 12:36:18 +0200

    init project

---
+++ adivinaLaPalabra/src/main/java/com/soprasteria/adivinaLaPalabra/AdivinaLaPalabraApplication.java
@@ -0,0 +1,13 @@
+package com.soprasteria.adivinaLaPalabra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdivinaLaPalabraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdivinaLaPalabraApplication.class, args);
+	}
+
+}
---
+++ adivinaLaPalabra/src/test/java/com/soprasteria/adivinaLaPalabra/AdivinaLaPalabraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soprasteria.adivinaLaPalabra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdivinaLaPalabraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
