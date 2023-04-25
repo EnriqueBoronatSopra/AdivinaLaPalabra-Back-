f0299257a8be138bad4b9ecdf2c0ec73e7a5bd4a
EnriqueBoronatSopra <ej.boronat@ext.soprasteria.com>
2023-04-25 11:48:17 +0200

    feat(NewRoundService) Added try/catch on database access and a log for the error

--- adivinaLaPalabra/src/main/java/com/soprasteria/adivinaLaPalabra/service/NewRoundServiceImpl.java
+++ adivinaLaPalabra/src/main/java/com/soprasteria/adivinaLaPalabra/service/NewRoundServiceImpl.java
@@ -4,10 +4,14 @@ import com.soprasteria.adivinaLaPalabra.config.WordsReader;
 import com.soprasteria.adivinaLaPalabra.dto.RoundResponse;
 import com.soprasteria.adivinaLaPalabra.model.RoundEntity;
 import com.soprasteria.adivinaLaPalabra.repository.IRoundEntityRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.dao.DataAccessResourceFailureException;
 
 public class NewRoundServiceImpl implements INewRoundService {
+
+    private static final Logger logger = LoggerFactory.getLogger(INewRoundService.class);
     @Autowired
     WordsReader wordsReader;
     @Autowired
@@ -15,14 +19,20 @@ public class NewRoundServiceImpl implements INewRoundService {
     @Override
     public RoundResponse newRound() {
         String wordToGuess = wordsReader.getRandomWord();
-        RoundResponse roundResponse = new RoundResponse();
+        RoundResponse roundResponse = null;
 
         RoundEntity roundEntity = new RoundEntity();
+        RoundEntity savedRoundEntity;
         roundEntity.setWord(wordToGuess);
-        RoundEntity savedRoundEntity = roundEntityRepository.save(roundEntity);
 
-        Long idRoundSaved = savedRoundEntity.getId();
-        roundResponse.setId(idRoundSaved);
+        try {
+            savedRoundEntity = roundEntityRepository.save(roundEntity);
+            roundResponse = new RoundResponse();
+            Long idRoundSaved = savedRoundEntity.getId();
+            roundResponse.setId(idRoundSaved);
+        } catch (DataAccessResourceFailureException exception) {
+            logger.error("Error saving the round in the Database");
+        }
         return roundResponse;
     }
 }
