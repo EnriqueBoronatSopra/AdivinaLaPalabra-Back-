3e87c0dc5ac17598b49b25152756b6c19eab6022
NereBS <131141706+NereBS@users.noreply.github.com>
2023-05-05 08:55:26 +0200

    feat(UserEntityAndRepository) UserEntity and UserRepository creation (#20)

--- adivinaLaPalabra/src/main/java/com/soprasteria/adivinalapalabra/model/RoundEntity.java
+++ adivinaLaPalabra/src/main/java/com/soprasteria/adivinalapalabra/model/RoundEntity.java
@@ -2,6 +2,8 @@ package com.soprasteria.adivinalapalabra.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.ToString;
+import org.apache.catalina.User;
 
 @Data
 @Entity
@@ -13,6 +15,11 @@ public class RoundEntity {
     private Long id;
     private String word;
     private int intentNumber;
+
+    @ManyToOne
+    @JoinColumn(name = "idUser")
+    @ToString.Exclude
+    private UserEntity user;
 
     public void incrementIntentInOne() {
         intentNumber++;
