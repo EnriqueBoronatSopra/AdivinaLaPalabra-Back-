1308e0b9dd262e0b85b2565208cea3ee55705a38
NereBS <131141706+NereBS@users.noreply.github.com>
2023-05-05 11:39:51 +0200

    feat(UserEntity) Changes in class User Entity and UserRepository (#22)
    * feat(UserEntity) Changes in class User Entity and UserRepository
    
    * feat(UserEntity) Changes in class User Entity and UserRepository

--- adivinaLaPalabra/src/main/java/com/soprasteria/adivinalapalabra/model/UserEntity.java
+++ adivinaLaPalabra/src/main/java/com/soprasteria/adivinalapalabra/model/UserEntity.java
@@ -1,9 +1,10 @@
 package com.soprasteria.adivinalapalabra.model;
 
+import com.soprasteria.adivinalapalabra.utils.ErrorMsgs;
 import jakarta.persistence.*;
 import lombok.Data;
-import lombok.ToString;
 
+import javax.naming.Context;
 import java.util.List;
 
 @Data
@@ -14,11 +15,15 @@ public class UserEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @Column(unique = true)
     private String name;
     private String password;
 
     @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "user")
     private List<RoundEntity> listRound;
+    
+
+
 
 
 }
