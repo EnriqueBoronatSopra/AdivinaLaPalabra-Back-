e51a7022e0590652ac72db8aa84db00a1f2bb269
EnriqueBoronatSopra <ej.boronat@ext.soprasteria.com>
2023-05-05 11:44:32 +0200

    feat(UserController) fix

--- adivinaLaPalabra/src/main/java/com/soprasteria/adivinalapalabra/service/LoginServiceImpl.java
+++ adivinaLaPalabra/src/main/java/com/soprasteria/adivinalapalabra/service/LoginServiceImpl.java
@@ -1,8 +1,12 @@
 package com.soprasteria.adivinalapalabra.service;
 
 import com.soprasteria.adivinalapalabra.dto.UserDto;
+import com.soprasteria.adivinalapalabra.model.UserEntity;
+import com.soprasteria.adivinalapalabra.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class LoginServiceImpl implements LoginService {
@@ -13,8 +17,8 @@ public class LoginServiceImpl implements LoginService {
     private UserRepository userRepository;
     @Override
     public String login(UserDto userDto) {
-        Optional<UserEntity> user = userRepository.findByName(userDto.getName()).stream().findFirst();
+        Optional<UserEntity> user = userRepository.findByName(userDto.getName());
 
-        return user.isEmpty || !user.get().getPassword.equals(userDto.getPassword())? INCORRECT_LOGIN : CORRECT_LOGIN;
+        return user.isEmpty() || !user.get().getPassword().equals(userDto.getPassword())? INCORRECT_LOGIN : CORRECT_LOGIN;
     }
 }
