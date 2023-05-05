42798f8d70765565f7b9fb8c2a83a1e6e00a7e75
NereBS <nerea.balibrea@ext.soprasteria.com>
2023-05-05 11:13:55 +0200

    feat(UserEntity) Changes in class User Entity and UserRepository

--- adivinaLaPalabra/src/main/java/com/soprasteria/adivinalapalabra/model/UserEntity.java
+++ adivinaLaPalabra/src/main/java/com/soprasteria/adivinalapalabra/model/UserEntity.java
@@ -1,9 +1,10 @@
 package com.soprasteria.adivinalapalabra.model;
 
+import com.soprasteria.adivinalapalabra.utils.ErrorMsgs;
 import jakarta.persistence.*;
 import lombok.Data;
-import lombok.ToString;
 
+import javax.naming.Context;
 import java.util.List;
 
 @Data
@@ -14,11 +15,24 @@ public class UserEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @Column(unique = true)
     private String name;
     private String password;
 
     @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "user")
     private List<RoundEntity> listRound;
+
+
+    public Boolean userUnique(String name){
+       boolean unique = true;
+       if (name.equals(this.name)) {
+           unique = false;
+       }
+       return unique;
+
+    }
+
+
 
 
 }
