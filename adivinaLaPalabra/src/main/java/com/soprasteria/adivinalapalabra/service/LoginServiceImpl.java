5f752829978129c40ac5b700073182df9f975a18
EnriqueBoronatSopra <ej.boronat@ext.soprasteria.com>
2023-05-08 09:57:17 +0200

    feat(LoginService) added necessary dependencies

--- adivinaLaPalabra/src/main/java/com/soprasteria/adivinalapalabra/service/LoginServiceImpl.java
+++ adivinaLaPalabra/src/main/java/com/soprasteria/adivinalapalabra/service/LoginServiceImpl.java
@@ -8,6 +8,7 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Base64;
 import java.util.Date;
 import java.util.Optional;
 
@@ -16,9 +17,11 @@ import static com.soprasteria.adivinalapalabra.utils.ErrorMsgs.INCORRECT_LOGIN;
 @Service
 public class LoginServiceImpl implements LoginService {
 
-    private static final String SECRET_KEY = "T4k3_Up23";
+    private static final String SECRET_WORD = "T4k3_Up23";
+
     @Autowired
     private UserRepository userRepository;
+
     @Override
     public String login(LoginRequest loginRequest) {
         Optional<UserEntity> user = userRepository.findByName(loginRequest.getName());
@@ -27,9 +30,12 @@ public class LoginServiceImpl implements LoginService {
             return INCORRECT_LOGIN;
         }
 
+        final String SECRET_KEY = Base64.getEncoder().encodeToString(SECRET_WORD.getBytes());
+        final int tokenValidity = 900000;
+
         return Jwts.builder()
                 .setSubject(user.get().getName())
-                .setExpiration(new Date(System.currentTimeMillis() + 900000))
+                .setExpiration(new Date(System.currentTimeMillis() + tokenValidity))
                 .signWith(SignatureAlgorithm.HS512,
                         SECRET_KEY.getBytes()).compact();
     }
--- adivinaLaPalabra/src/test/java/com/soprasteria/adivinalapalabra/controller/LoginControllerTest.java
+++ adivinaLaPalabra/src/test/java/com/soprasteria/adivinalapalabra/controller/LoginControllerTest.java
@@ -2,17 +2,28 @@ package com.soprasteria.adivinalapalabra.controller;
 
 import com.soprasteria.adivinalapalabra.dto.LoginRequest;
 import com.soprasteria.adivinalapalabra.service.LoginServiceImpl;
+import io.jsonwebtoken.CompressionCodecs;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.Date;
+
+import static com.soprasteria.adivinalapalabra.utils.ErrorMsgs.INCORRECT_LOGIN;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
 public class LoginControllerTest {
+
+    private static final String SECRET_WORD = "T4k3_Up23";
+    private static final String SECRET_KEY = Base64.getEncoder().encodeToString(SECRET_WORD.getBytes());
     @Mock
     private LoginServiceImpl loginService;
 
@@ -21,26 +32,31 @@ public class LoginControllerTest {
 
     @Test
     void correctLoginTest() {
-        String requestExpected = "Correct login";
         String name = "name";
         String password = "password";
+        final int tokenValidity = 900000;
         LoginRequest loginRequest = new LoginRequest(name, password);
+        String token = Jwts.builder()
+                .setSubject(name)
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .setExpiration(new Date(System.currentTimeMillis() + tokenValidity))
+                .compact();
 
-        when(loginService.login(loginRequest)).thenReturn(LoginServiceImpl.CORRECT_LOGIN);
+        when(loginService.login(loginRequest)).thenReturn(token);
 
         String request = loginService.login(loginRequest);
 
-        assertEquals(requestExpected, request);
+        assertEquals(token, request);
     }
 
     @Test
     void incorrectLoginTest() {
-        String requestExpected = "Incorrect login";
+        String requestExpected = "User or password incorrect";
         String name = "name";
         String password = "password";
         LoginRequest loginRequest = new LoginRequest(name, password);
 
-        when(loginService.login(loginRequest)).thenReturn(LoginServiceImpl.INCORRECT_LOGIN);
+        when(loginService.login(loginRequest)).thenReturn(INCORRECT_LOGIN);
 
         String request = loginService.login(loginRequest);
 
