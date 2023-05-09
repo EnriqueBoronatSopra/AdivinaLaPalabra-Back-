850650ea521df10ee7d8f42e96834bdb082930e3
NereBS <nerea.balibrea@ext.soprasteria.com>
2023-05-09 11:14:23 +0200

    feat(aplication.properties) Insert base data when starting spring boot

--- adivinaLaPalabra/src/main/resources/sql/data.sql
+++ adivinaLaPalabra/src/main/resources/sql/data.sql
@@ -1,10 +1,10 @@
 
-INSERT INTO `adivinalapalabra`.`user` SELECT 1,'maria','mariabd34'
-WHERE NOT EXISTS(SELECT 1 FROM `user` WHERE name = 'maria');
+INSERT INTO `adivinalapalabra`.`user` SELECT 1,'maria','maria123'
+WHERE NOT EXISTS(SELECT * FROM `user` WHERE id = 1);
 INSERT INTO `adivinalapalabra`.`user` SELECT 2,'fran','fran456'
-WHERE NOT EXISTS(SELECT 2 FROM `user` WHERE name = 'fran');
+WHERE NOT EXISTS(SELECT * FROM `user` WHERE id = 2);
 INSERT INTO `adivinalapalabra`.`user` SELECT 3,'pepe','pepe789'
-WHERE NOT EXISTS(SELECT 3 FROM `user` WHERE name = 'pepe');
+WHERE NOT EXISTS(SELECT * FROM `user` WHERE id = 3);
 COMMIT;
 
 
