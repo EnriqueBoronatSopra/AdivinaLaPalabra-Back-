ca1e90ecf4d2f4d82a9a5947d5812ae35ed0e562
NereBS <nerea.balibrea@ext.soprasteria.com>
2023-05-09 11:27:17 +0200

    feat(aplication.properties) Insert base data when starting spring boot

--- adivinaLaPalabra/src/main/java/com/soprasteria/adivinalapalabra/AdivinaLaPalabraApplication.java
+++ adivinaLaPalabra/src/main/java/com/soprasteria/adivinalapalabra/AdivinaLaPalabraApplication.java
@@ -1,10 +1,7 @@
 package com.soprasteria.adivinalapalabra;
 
-import com.soprasteria.adivinalapalabra.model.UserEntity;
-import com.soprasteria.adivinalapalabra.repository.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class AdivinaLaPalabraApplication {
--- adivinaLaPalabra/src/main/resources/sql/data.sql
+++ adivinaLaPalabra/src/main/resources/sql/data.sql
@@ -1,10 +1,10 @@
 
 INSERT INTO `adivinalapalabra`.`user` SELECT 1,'maria','maria123'
-WHERE NOT EXISTS(SELECT * FROM `user` WHERE id = 1);
+ WHERE NOT EXISTS(SELECT * FROM `user` WHERE id = 1);
 INSERT INTO `adivinalapalabra`.`user` SELECT 2,'fran','fran456'
-WHERE NOT EXISTS(SELECT * FROM `user` WHERE id = 2);
+ WHERE NOT EXISTS(SELECT * FROM `user` WHERE id = 2);
 INSERT INTO `adivinalapalabra`.`user` SELECT 3,'pepe','pepe789'
-WHERE NOT EXISTS(SELECT * FROM `user` WHERE id = 3);
+ WHERE NOT EXISTS(SELECT * FROM `user` WHERE id = 3);
 COMMIT;
 
 
