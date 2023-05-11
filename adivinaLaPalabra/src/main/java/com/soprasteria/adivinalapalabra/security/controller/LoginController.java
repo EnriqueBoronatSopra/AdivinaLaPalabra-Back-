22c2719981182643d25aac4a7fe3d443443ec340
EnriqueBoronatSopra <130563500+EnriqueBoronatSopra@users.noreply.github.com>
2023-05-11 08:45:37 +0200

    Security (#26)
    * feat(login) Changed controller to receive the base64-encoded name and password
    
    * feat(LoginController) refactor last commit
    
    * feat(LoginController) refactor last commit

--- adivinaLaPalabra/src/main/java/com/soprasteria/adivinalapalabra/security/controller/LoginController.java
+++ adivinaLaPalabra/src/main/java/com/soprasteria/adivinalapalabra/security/controller/LoginController.java
@@ -3,6 +3,8 @@ package com.soprasteria.adivinalapalabra.security.controller;
 import com.soprasteria.adivinalapalabra.security.dto.JwtDto;
 import com.soprasteria.adivinalapalabra.security.dto.LoginRequest;
 import com.soprasteria.adivinalapalabra.security.jwt.JWTProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -10,6 +12,8 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Base64;
 
 @RestController
 @CrossOrigin
@@ -19,13 +23,15 @@ public class LoginController {
     @Autowired
     AuthenticationManager authenticationManager;
 
+    private final static Logger logger = LoggerFactory.getLogger(LoginController.class);
+
     @Autowired
     JWTProvider jwtProvider;
 
     @PostMapping
     public ResponseEntity<JwtDto> login(@RequestBody LoginRequest loginRequest) {
-        String name = loginRequest.getName();
-        String password = loginRequest.getPassword();
+        String name = decodedString(loginRequest.getName());
+        String password = decodedString(loginRequest.getPassword());
         Authentication authentication =
                 authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(name, password));
         SecurityContextHolder.getContext().setAuthentication(authentication);
@@ -33,5 +39,15 @@ public class LoginController {
         JwtDto jwtDto = new JwtDto(jwt);
 
         return ResponseEntity.ok(jwtDto);
+    }
+
+    private String decodedString(String encodedString) {
+        try {
+            byte[] stringDecodedBytes = Base64.getDecoder().decode(encodedString);
+            return new String(stringDecodedBytes);
+        } catch (IllegalArgumentException exception) {
+            logger.error("Error with bytes received");
+        }
+        return null;
     }
 }
